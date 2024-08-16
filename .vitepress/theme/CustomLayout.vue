f90f01ea831c8a9383e53a46858d67effacb8a5a
chengliang <chengliang4810@163.com>
2024-08-16 22:11:36 +0800

    pref: 自定义 layout 预留组件入口

---
+++ .vitepress/theme/CustomLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <!-- 右侧导航上方 -->
+    <template #aside-outline-before>
+
+    </template>
+  </Layout>
+</template>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,11 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme'
+import CustomLayout from './CustomLayout.vue'
+
 import './custom.css'
 
-export default DefaultTheme
+export default {
+    extends: DefaultTheme,
+    // 使用注入插槽的包装组件覆盖 Layout
+    Layout: CustomLayout
+}
